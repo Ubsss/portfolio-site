feef1bebf3a5db36d5ae651ed5630dc5df6139e4
uboh <ubohchuks@gmail.com>
2020-02-09 20:11:30 -0600

    added build folder

---
+++ build/precache-manifest.e7fb55c9128ed93b8b29fc7d80e8dfc4.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6db7ddd6293116860812ddbcde7180",
+    "url": "/portfolio-site/index.html"
+  },
+  {
+    "revision": "667035c659968f428201",
+    "url": "/portfolio-site/static/css/2.b83c4205.chunk.css"
+  },
+  {
+    "revision": "05aa9dbf6c19369866de",
+    "url": "/portfolio-site/static/css/main.dcb87be8.chunk.css"
+  },
+  {
+    "revision": "667035c659968f428201",
+    "url": "/portfolio-site/static/js/2.b5066511.chunk.js"
+  },
+  {
+    "revision": "a0ba84ce1227f2b6d4618b641a1886b6",
+    "url": "/portfolio-site/static/js/2.b5066511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05aa9dbf6c19369866de",
+    "url": "/portfolio-site/static/js/main.4942ff7e.chunk.js"
+  },
+  {
+    "revision": "9a1309d5e21d94c44755",
+    "url": "/portfolio-site/static/js/runtime-main.8cfed093.js"
+  },
+  {
+    "revision": "13f79d1fb92f130327434f44b966716d",
+    "url": "/portfolio-site/static/media/algorithm.13f79d1f.jpeg"
+  },
+  {
+    "revision": "30d8879f6268e2a2bcdd011e6ab24996",
+    "url": "/portfolio-site/static/media/bootstrap.30d8879f.svg"
+  },
+  {
+    "revision": "a0a89ee73da69b3b720e2a641745c41e",
+    "url": "/portfolio-site/static/media/cpp.a0a89ee7.jpeg"
+  },
+  {
+    "revision": "aac116a20cc1459d0788cfc067a7751f",
+    "url": "/portfolio-site/static/media/docker.aac116a2.svg"
+  },
+  {
+    "revision": "cdc525fef1db60b5ba5c2524d80a06ce",
+    "url": "/portfolio-site/static/media/express.cdc525fe.svg"
+  },
+  {
+    "revision": "0d57335f8214435469b529c651e9709f",
+    "url": "/portfolio-site/static/media/github.0d57335f.svg"
+  },
+  {
+    "revision": "205068047e7ce138b0bad9c2cba4bf5c",
+    "url": "/portfolio-site/static/media/github.20506804.svg"
+  },
+  {
+    "revision": "c50d87a4901d265e940eab5dbc2fd581",
+    "url": "/portfolio-site/static/media/link.c50d87a4.svg"
+  },
+  {
+    "revision": "de23bc7cd73badf09ef236f507b10149",
+    "url": "/portfolio-site/static/media/linkedin.de23bc7c.svg"
+  },
+  {
+    "revision": "705b987e58fa317cf913c181018040be",
+    "url": "/portfolio-site/static/media/node.705b987e.svg"
+  },
+  {
+    "revision": "27d5b8b0a7c155508e945972263a66ab",
+    "url": "/portfolio-site/static/media/portfolio.27d5b8b0.jpg"
+  },
+  {
+    "revision": "efbacac9ecf67d55558993c9554ec3c5",
+    "url": "/portfolio-site/static/media/profilePhoto.efbacac9.jpeg"
+  },
+  {
+    "revision": "1f6a76a4bf66ee1d6bdeab1edc5e2b5c",
+    "url": "/portfolio-site/static/media/react.1f6a76a4.svg"
+  },
+  {
+    "revision": "3e3945f404f289a8107547791307bb5c",
+    "url": "/portfolio-site/static/media/redux.3e3945f4.svg"
+  },
+  {
+    "revision": "40175d24e44e28b4422556e8eb71a04c",
+    "url": "/portfolio-site/static/media/rightArrow1.40175d24.svg"
+  },
+  {
+    "revision": "8a27086bad036a2aeb1928bc8fa0cbfb",
+    "url": "/portfolio-site/static/media/robots.8a27086b.jpg"
+  },
+  {
+    "revision": "79bee3374097c970740c572ad14a69cd",
+    "url": "/portfolio-site/static/media/stack.79bee337.svg"
+  },
+  {
+    "revision": "073bf06c5535302b6549924b90ccc7ca",
+    "url": "/portfolio-site/static/media/toTopArrow.073bf06c.svg"
+  },
+  {
+    "revision": "400f36b7e82f70d6c35bcd918909b31e",
+    "url": "/portfolio-site/static/media/travis.400f36b7.svg"
+  }
+]);
